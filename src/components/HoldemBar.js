d383870627f0f5646df55fc91f4061a5318fb453
tsaohucn <tsaohucn@gmail.com>
2018-07-11 19:58:15 +0700

    add icon function

--- src/components/HoldemBar.js
+++ src/components/HoldemBar.js
@@ -15,18 +15,13 @@ import MenuIcon from '@material-ui/icons/Menu';
 import PersonIcon from '@material-ui/icons/Person';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import renderToolList from './renderToolList';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import StarIcon from '@material-ui/icons/Star';
-import SendIcon from '@material-ui/icons/Send';
-import MailIcon from '@material-ui/icons/Mail';
-import DeleteIcon from '@material-ui/icons/Delete';
-import ReportIcon from '@material-ui/icons/Report';
+// local components
+import Icon from './Icon'
 import firebase from '../configs/firebase'
+import config from '../configs/config'
 
 const drawerWidth = 240;
 
@@ -157,42 +152,16 @@ class PersistentDrawer extends React.Component {
         </div>
         <Divider />
         <List>
-          <ListItem button onClick={() => { this.props.alert.show('Oh look, an alert!') }}>
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary="俱樂部資料表" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <StarIcon />
-            </ListItemIcon>
-            <ListItemText primary="裁判資料表" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <SendIcon />
-            </ListItemIcon>
-            <ListItemText primary="會員資料表" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <DraftsIcon />
-            </ListItemIcon>
-            <ListItemText primary="員工資料表" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <DeleteIcon />
-            </ListItemIcon>
-            <ListItemText primary="報表" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <ReportIcon />
-            </ListItemIcon>
-            <ListItemText primary="桌次現況" />
-          </ListItem>
+          {
+            config['tool'].map( ele => (
+              <ListItem button>
+                <ListItemIcon>
+                  <Icon tag={ele.icon}/>
+                </ListItemIcon>
+                <ListItemText primary={ele.title} />
+              </ListItem>
+            ))
+          }
         </List>
       </Drawer>
     );
