66fc64d250a49bd23f442cfe1745c41d21647388
Tsao <tsaohucn@gmail.com>
2018-07-12 03:58:37 +0700

    modify UI

--- src/screens/mains/ClubScreen.js
+++ src/screens/mains/ClubScreen.js
@@ -1,8 +1,9 @@
 // node_module
-import React from 'react';
+import React from 'react'
 import SearchBar from 'material-ui-search-bar'
-import SearchIcon from '@material-ui/icons/Search';
-import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 // local components
 import withHoldemBar from '../../hocs/withHoldemBar'
 
@@ -11,6 +12,7 @@ class ClubScreen extends React.Component {
 	constructor(props) {
     super(props)
     this.state = {
+      page: 1,
     	search: ''
     }
 	}
@@ -21,25 +23,85 @@ class ClubScreen extends React.Component {
 
   onChange = (newValue) => this.setState({ search: newValue })
 
+  goToAddNewClub = () => {
+    this.setState({
+      page: 2
+    })
+  }
+
+  addNewClub = () => {
+    this.setState({
+      page: 1
+    })    
+  }
+
+  renderContent = () => {
+    if (this.state.page === 2) {
+      return(
+        <div>
+          <div>
+            <TextField
+              label="俱樂部名稱"
+              id="margin-normal"
+              //defaultValue="club"
+              style={styles.textField}
+              //helperText="俱樂部名稱"
+              margin="normal"
+            />
+          </div>
+          <div>
+            <TextField
+              label="俱樂部帳號"
+              id="margin-normal"
+              //defaultValue="account"
+              style={styles.textField}
+              //helperText="俱樂部帳號"
+              margin="normal"
+            />
+          </div>
+          <div>
+            <TextField
+              label="俱樂部密碼"
+              id="margin-normal"
+              //defaultValue="12345"
+              style={styles.textField}
+              //helperText="俱樂部密碼"
+              margin="normal"
+            />
+          </div>
+          <Button style={styles.button} variant="contained" color="secondary" onClick={this.addNewClub}>確認新增俱樂部</Button>
+        </div>
+      )
+    } else {
+      return(
+        <div>
+          <h1>俱樂部查詢</h1>
+          <div style={styles.searchView}>
+            <SearchBar
+              style={styles.SearchBar}
+              value={this.state.search}
+              onChange={this.onChange}
+              onRequestSearch={this.searchClub}
+            />
+          </div>
+          <br/>
+          <div style={styles.buttonView}>
+            <div style={styles.buttonSlit}>
+              <Button style={styles.button} variant="contained" color="secondary">搜索</Button>
+              <Button style={styles.button} variant="contained" color="secondary" onClick={this.goToAddNewClub}>新增俱樂部</Button>
+            </div>
+          </div> 
+        </div>
+      )    
+    }
+  }
+
   render() {
     return(
-    	<div style={styles.container}>
-        <h1>俱樂部查詢</h1>
-        <div style={styles.searchView}>
-  			  <SearchBar
-            style={styles.SearchBar}
-  			    value={this.state.search}
-  			    onChange={this.onChange}
-  			    onRequestSearch={this.searchClub}
-  			  />
-        </div>
-        <br/>
-        <div style={styles.buttonView}>
-          <div style={styles.buttonSlit}>
-            <Button style={styles.button} variant="contained" color="secondary">搜索</Button>
-            <Button style={styles.button} variant="contained" color="secondary">新增俱樂部</Button>
-          </div>
-        </div>
+    	<div>
+       {
+        this.renderContent()
+       }
 		  </div>
     )
   }
@@ -70,9 +132,12 @@ const styles = {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-    width: '20%'
+    width: '35%',
   },
   button: {
+    width: '40%'
+  },
+  textField: {
     width: '40%'
   }
 }
