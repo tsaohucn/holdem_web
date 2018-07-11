7defc11c9d1685950a942ff8a4ef4fa430cec01e
tsaohucn <tsaohucn@gmail.com>
2018-07-12 01:11:58 +0700

    add searchBar

--- src/screens/mains/ClubScreen.js
+++ src/screens/mains/ClubScreen.js
@@ -1,13 +1,34 @@
 // node_module
 import React from 'react';
+import SearchBar from 'material-ui-search-bar'
+import SearchIcon from '@material-ui/icons/Search';
 // local components
 import withHoldemBar from '../../hocs/withHoldemBar'
 
 class ClubScreen extends React.Component {
 
+	constructor(props) {
+    super(props)
+    this.state = {
+    	search: ''
+    }
+	}
+
+  searchClub() {
+  	//
+  }
+
   render() {
     return(
-      <h1>{'ClubScreen'}</h1>
+    	<div style={{display: 'flex', flexDirection: 'row'}}>
+    	  <h1>俱樂部查詢</h1>
+			  <SearchBar
+			    value={this.state.search}
+			    onChange={(newValue) => this.setState({ search: newValue })}
+			    onRequestSearch={this.searchClub}
+			    closeIcon={<SearchIcon/>}
+			  />
+		  </div>
     )
   }
 }
