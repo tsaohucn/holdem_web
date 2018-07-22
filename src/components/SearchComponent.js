281b6cdb49295612b8f9bc9c9b6527e97e12368c
Tsao <tsaohucn@gmail.com>
2018-07-23 01:46:36 +0700

    complete memberCount

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -29,7 +29,7 @@ class SearchComponent extends PureComponent {
             style={styles.SearchBar}
             value={''}
             //onChange={this.onChange}
-            //onRequestSearch={this.searchClub}
+            onRequestSearch={() => {}}
           />
         </div>
         <br/>
@@ -55,7 +55,7 @@ class SearchComponent extends PureComponent {
                 style={styles.SearchBar}
                 value={''}
                 //onChange={this.onChange}
-                //onRequestSearch={this.searchClub}
+                onRequestSearch={() => {}}
               /> 
             </div>
             <br/> 
