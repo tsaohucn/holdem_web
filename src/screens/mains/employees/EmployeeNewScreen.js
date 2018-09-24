01ea9768c64435fd83783828bffc0567d40d0c32
Tsao <tsaohucn@gmail.com>
2018-09-24 19:43:22 +0800

    rename

--- src/screens/mains/employees/EmployeeNewScreen.js
+++ src/screens/mains/employees/EmployeeNewScreen.js
@@ -3,10 +3,10 @@ import React from 'react'
 import { withAlert } from 'react-alert'
 // local components
 import withNavigation from '../../../hocs/withNavigation'
-import withField from '../../../hocs/withField'
+import withForm from '../../../hocs/withForm'
 import ui from '../../../configs/ui'
 
-const EmployeeNewScreen = withField({
+const EmployeeNewScreen = withForm({
   field: ui.employeesField,
   buttonTitle: '送出',
   resource: 'employees',
