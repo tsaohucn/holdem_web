454c60d2317ef30f8f01dc5b356adee36878cbfe
Tsao <tsaohucn@gmail.com>
2018-10-22 16:46:52 +0800

    modify field name

--- src/hocs/withReport.js
+++ src/hocs/withReport.js
@@ -39,125 +39,21 @@ function withReport(params) {
       },async () => {
         try {
           await sleep(500)
-          if (router === 'day') {
-            const data = ['t','t'].map(ele =>({
-              date: this.date,
-              referee: '大笨蛋',
-              referee_day_report_table_id: 'A123',
-              totalC: '200',
-              gameMoney: '200',
-              totalI: '200',
-              totalT: '200',
-              totalMembersSpendTime: '200',
-              refereeId: 'A1233',
-              totalPlayerTime: '200',
-              totalPlayerMoney: '333',
-              rk: 4,
-              forClub: '200',
-              forReferee: '300',
-              st: 200
-            }))
-            this.setState({
-              isLoading: false,
-              data
-            })         
-          } else {
-            if (resource === 'members') {
-              const snap = await firebase.database().ref('members_report/').once('value')
-              if (snap.val()) {
-                const data = Object.values(snap.val())
-                this.setState({
-                  isLoading: false,
-                  data
-                })     
-              }
-            } else if (resource === 'referees') {
-              const moment = extendMoment(Moment)
-              const start = moment(this.startDate, 'YYYY-MM-DD')
-              const end   = moment(this.endDate, 'YYYY-MM-DD')
-              const range = moment.range(start, end)
-              const days = Array.from(range.by('day', { excludeEnd: true })).map(ele => ele.format('YYYY-MM-DD'))
-              const data = days.map(ele => ({
-                referee_report_date: ele,
-                rk: '10',
-                rk50: '200',
-                st: 'st',
-                returnClub: '中和俱樂部'
-              }))
-              this.setState({
-                isLoading: false,
-                data
-              }) 
-            } else if (resource === 'sales') {
-              const data = ['t','t'].map(ele =>({
-                date: this.startDate,
-                id: 'S123',
-                memberName: '大頭',
-                referee: '阿呆裁判',
-                level: '20/40',
-                gameTime: '200',
-                time: '1:11',
-                score: '200'
-              }))
-              this.setState({
-                isLoading: false,
-                data
-              })              
-            } else {
-              //
-            }
-          }
+          this.setState({
+            isLoading: false     
+          })
         } catch (error) {
           errorAlert(this.props.alert,'載入失敗 : ' + error.toString())
         } finally {
           //
         }
       })
-      /*
-      if (this.id === '$all') {
-        this.fetchTableData(firebase.database().ref(resource))
-      } else {
-        this.fetchTableData(firebase.database().ref(resource).orderByChild('id').equalTo(this.id))
-      }*/
     }
-/*
-    fetchTableData = async (fetch) => {
-      let data_arr = []
-      let id_names = {}
-      try {
-        await sleep(500)
-        const snap = fetch && (await fetch.once('value'))
-        const data = (snap && snap.val()) || {}
-        data_arr = Object.values(data) || []
-        const resource_keys = belong.map(belongResource => {
-          return data_arr.map(ele => {
-            return ele[belongResource]
-          })
-        }).flat()
-        const uniq_resource_keys = resource_keys.filter(ele => ele).filter((elem, pos, arr) => {
-          return arr.indexOf(elem) == pos
-        }) // uniq
-        const id_names_promise = uniq_resource_keys.map(key => firebase.database().ref('id_names/' + key).once('value'))
-        const id_names_snap = await Promise.all(id_names_promise)
-        uniq_resource_keys.forEach((key,index) => {
-          id_names[key] = id_names_snap[index].val()
-        })
-        this.setState({
-          isLoading: false,
-          data: data_arr,
-          id_names
-        }) 
-      } catch(err) {
-        errorAlert(this.props.alert,'載入失敗 : ' + err.toString())
-      } finally {
-        //
-      }      
-    }
-*/
+
     goBack = () => {
       this.props.history.goBack()
     }
-
+/*
     goToDateReport = (date) => {
       this.props.history.push('/reports/referee/day/' + date)
     }
@@ -165,7 +61,7 @@ function withReport(params) {
     goToMemberReport = (date,id) => {
       this.props.history.push('/reports/member/' + date + '/' + date + '/' + id)
     }
-
+*/
     render() {
 
       const Component = wrapperComponent ? wrapperComponent : PartialTableTwo
@@ -186,8 +82,8 @@ function withReport(params) {
               {...this.state}
               title={title}
               onClickTableReturnButton={this.goBack}
-              onClickDate={this.goToDateReport}
-              onClickTableId={this.goToMemberReport}
+              //onClickDate={this.goToDateReport}
+              //onClickTableId={this.goToMemberReport}
             />
           }
         </div>
