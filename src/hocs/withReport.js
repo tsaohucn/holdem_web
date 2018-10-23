e55e00229a0d51b34985d5dc2834eab5dcdd5329
Tsao <tsaohucn@gmail.com>
2018-10-24 01:01:48 +0800

    report date filter

--- src/hocs/withReport.js
+++ src/hocs/withReport.js
@@ -28,10 +28,8 @@ function withReport(params) {
 
     componentDidMount() {
       const searchValue = this.props.match.params.searchValue
-      const startDate = this.props.match.params.startDate
-      const endDate = this.props.match.params.endDate
       if (searchValue) {
-        this.fetchTableData(firebase.database().ref('reports').orderByChild('club_id_member_id').equalTo(searchValue))
+        this.fetchTableData(firebase.database().ref('reports').orderByChild('club_id_member_id').equalTo(this.props.HoldemStore.clubId + '_' + searchValue))
       } else {
         errorAlert(this.props.alert,'載入資料發生錯誤')
       }
@@ -43,12 +41,19 @@ function withReport(params) {
       },async () => {
         try {
           await sleep(500)
+          const startDate = this.props.match.params.startDate
+          const endDate = this.props.match.params.endDate
+          const moment = extendMoment(Moment)
+          const start = moment(startDate, 'YYYY-MM-DD')
+          const end   = moment(endDate, 'YYYY-MM-DD')
+          const range = Array.from(moment.range(start, end).by('day', { excludeEnd: false })).map(m => m.format('YYYY/MM/DD'))
           const snap = fetch && (await fetch.once('value'))
           const val = (snap && snap.val()) || {}
           const data = Object.values(val) || []
+          const in_range_data = data.filter(ele => range.includes(ele.playerDate))
           this.setState({
             isLoading: false,
-            data
+            data: in_range_data
           }) 
         } catch(err) {
           errorAlert(this.props.alert,'載入資料發生錯誤 : ' + err.toString())
