89bd1b01bad11c447fcd81953a49eed6324aa012
Tsao <tsaohucn@gmail.com>
2018-10-24 03:10:58 +0800

    add quit field

--- src/hocs/withEdit.js
+++ src/hocs/withEdit.js
@@ -77,36 +77,52 @@ function withEdit(params) {
       },async () => {
         try {
           await sleep(500)
-          if (resource === 'clubs' || resource === 'employees' || resource === 'referees' || resource === 'sales' || resource === 'members') {
-            // 先檢查帳號有無重複
-            if (data.account) {
-              const account_snap = await firebase.database().ref('/backends').orderByChild('account').equalTo(data.account).once('value')
-              if (account_snap.val() && (this.account != data.account)) {
-                throw '此帳號已有人使用'
+          if (this.key && this.account && this.password) {
+            if (resource === 'clubs' || resource === 'employees' || resource === 'referees' || resource === 'sales' || resource === 'members') {
+              // 先檢查帳號有無重複
+              if (data.account) {
+                const account_snap = await firebase.database().ref('/backends').orderByChild('account').equalTo(data.account).once('value')
+                if (account_snap.val() && (this.account != data.account)) {
+                  throw '此帳號已有人使用'
+                }
               }
-            }
-            if (resource === 'employees') {
-              await firebase.auth().signInWithEmailAndPassword(this.account,this.password)
-              const user = firebase.auth().currentUser
-              if (this.account != data.account) {
-                await user.updateEmail(data.account)
+              // 檢查是否真的有此使用者
+              /*
+              const snap = await firebase.database().ref(resource + '/' + this.key).once('value')
+              const val = snap.val()
+              if (val) {
+                if (val.table_id) {
+                  throw '此使用者正在遊戲中'
+                }
+              } else {
+                throw '查無此使用者'
+              }*/
+              // 更新資料
+              if (resource === 'employees') {
+                await firebase.auth().signInWithEmailAndPassword(this.account,this.password)
+                const user = firebase.auth().currentUser
+                if (this.account != data.account) {
+                  await user.updateEmail(data.account)
+                }
+                if (this.password != data.password) {
+                  await user.updatePassword(data.password)
+                }
               }
-              if (this.password != data.password) {
-                await user.updatePassword(data.password)
+              if (resource === 'clubs' || resource === 'employees' || resource === 'referees' || resource === 'sales') {
+                await firebase.database().ref('/backends/' + this.key).update({
+                  account: data.account,
+                  password: data.password
+                })
               }
-            }
-            if (resource === 'clubs' || resource === 'employees' || resource === 'referees' || resource === 'sales') {
-              this.key && await firebase.database().ref('/backends/' + this.key).update({
-                account: data.account,
-                password: data.password
+              const upload_data = Object.assign({},data,{
+                club_id_name: data.club_id + '_' + data.name
               })
+              await firebase.database().ref(resource + '/' + this.key).update(upload_data)
+            } else {
+              throw '資源錯誤'
             }
-            const upload_data = Object.assign({},data,{
-              club_id_name: data.club_id + '_' + data.name
-            })
-            this.key && await firebase.database().ref(resource + '/' + this.key).update(upload_data)
           } else {
-            throw '資源錯誤'
+            throw '系統錯誤'
           }
           successAlert(this.props.alert,'更新成功')
           this.goBack()
@@ -128,87 +144,107 @@ function withEdit(params) {
       },async () => {
         try {
           await sleep(500)
-          if (resource === 'clubs' || resource === 'employees' || resource === 'referees' || resource === 'sales' || resource === 'members') {
-            const refereeCount = data.refereeCount
-            const saleCount = data.saleCount
-            const memberCount = data.memberCount
-            const employeeCount = data.employeeCount
-            // 檢查底下有無人
-            if (employeeCount > 0 || refereeCount > 0 || saleCount > 0 || memberCount > 0) {
-              throw '此人底下存在會員'
-            } 
-            // 刪資料
-            if (resource === 'employees') {
-              await firebase.auth().signInWithEmailAndPassword(this.account,this.password)
-              const user = firebase.auth().currentUser
-              await user.delete()              
-            }
-            // non_use
-            //await firebase.database().ref('nonuse_' + resource + '/' + this.key).update(data)
-            //if (resource === 'clubs' || resource === 'employees' || resource === 'referees' || resource === 'sales') {
-            //  await firebase.database().ref('/backends/' + this.key).update({
-            //    nonuse: true
-            //  })
-            //}
-            this.key && await firebase.database().ref('/backends/' + this.key).remove()
-            resource && this.key && await firebase.database().ref(resource + '/' + this.key).remove()
-            // count
-            if (resource === 'employees' || resource === 'referees' || resource === 'sales' || resource === 'members') {
-              switch(resource) {
-                case 'employees':
-                await firebase.database().ref('clubs/' +  data['club_key'] + '/employeeCount').transaction(count => {
-                  if (count) {
-                    return count - 1
-                  } else {
-                    return 0
-                  }
-                })
-                break
-                case 'referees':
-                await firebase.database().ref('clubs/' +  data['club_key'] + '/refereeCount').transaction(count => {
-                  if (count) {
-                    return count - 1
-                  } else {
-                    return 0
-                  }
-                })
-                break
-                case 'sales':
-                await firebase.database().ref('clubs/' +  data['club_key'] + '/saleCount').transaction(count => {
-                  if (count) {
-                    return count - 1
-                  } else {
-                    return 0
-                  }
-                })
-                break
-                case 'members':
-                await firebase.database().ref('clubs/' +  data['club_key'] + '/memberCount').transaction(count => {
-                  if (count) {
-                    return count - 1
-                  } else {
-                    return 0
-                  }
-                })
-                await firebase.database().ref('referees/' +  data['referee_key'] + '/memberCount').transaction(count => {
-                  if (count) {
-                    return count - 1
-                  } else {
-                    return 0
-                  }
-                })
-                await firebase.database().ref('sales/' +  data['sale_key'] + '/memberCount').transaction(count => {
-                  if (count) {
-                    return count - 1
-                  } else {
-                    return 0
-                  }
-                })
-                break
+          if (this.key && this.account && this.password) {
+            if (resource === 'clubs' || resource === 'employees' || resource === 'referees' || resource === 'sales' || resource === 'members') {
+              const refereeCount = data.refereeCount
+              const saleCount = data.saleCount
+              const memberCount = data.memberCount
+              const employeeCount = data.employeeCount
+              // 檢查底下有無人
+              if (employeeCount > 0 || refereeCount > 0 || saleCount > 0 || memberCount > 0) {
+                throw '此人底下存在會員'
+              } 
+              // 檢查是否真的有此使用者
+              /*
+              const snap = await firebase.database().ref(resource + '/' + this.key).once('value')
+              const val = snap.val()
+              if (val) {
+                if (val.table_id) {
+                  throw '此使用者正在遊戲中'
+                }
+              } else {
+                throw '查無此使用者'
               }
+              */
+              // 刪資料
+              if (resource === 'employees') {
+                await firebase.auth().signInWithEmailAndPassword(this.account,this.password)
+                const user = firebase.auth().currentUser
+                await user.delete()              
+              }
+              // non_use
+              //await firebase.database().ref('nonuse_' + resource + '/' + this.key).update(data)
+              //if (resource === 'clubs' || resource === 'employees' || resource === 'referees' || resource === 'sales') {
+              //  await firebase.database().ref('/backends/' + this.key).update({
+              //    nonuse: true
+              //  })
+              //}
+              await firebase.database().ref('/backends/' + this.key).remove()
+              // 改成加已刪除標記
+              await firebase.database().ref(resource + '/' + this.key).update({
+                quit: true
+              })
+              //await firebase.database().ref(resource + '/' + this.key).remove()
+              // count
+              if (resource === 'employees' || resource === 'referees' || resource === 'sales' || resource === 'members') {
+                switch(resource) {
+                  case 'employees':
+                  await firebase.database().ref('clubs/' +  data['club_key'] + '/employeeCount').transaction(count => {
+                    if (count) {
+                      return count - 1
+                    } else {
+                      return 0
+                    }
+                  })
+                  break
+                  case 'referees':
+                  await firebase.database().ref('clubs/' +  data['club_key'] + '/refereeCount').transaction(count => {
+                    if (count) {
+                      return count - 1
+                    } else {
+                      return 0
+                    }
+                  })
+                  break
+                  case 'sales':
+                  await firebase.database().ref('clubs/' +  data['club_key'] + '/saleCount').transaction(count => {
+                    if (count) {
+                      return count - 1
+                    } else {
+                      return 0
+                    }
+                  })
+                  break
+                  case 'members':
+                  await firebase.database().ref('clubs/' +  data['club_key'] + '/memberCount').transaction(count => {
+                    if (count) {
+                      return count - 1
+                    } else {
+                      return 0
+                    }
+                  })
+                  await firebase.database().ref('referees/' +  data['referee_key'] + '/memberCount').transaction(count => {
+                    if (count) {
+                      return count - 1
+                    } else {
+                      return 0
+                    }
+                  })
+                  await firebase.database().ref('sales/' +  data['sale_key'] + '/memberCount').transaction(count => {
+                    if (count) {
+                      return count - 1
+                    } else {
+                      return 0
+                    }
+                  })
+                  break
+                }
+              }
+            } else {
+              throw '資源錯誤'
             }
           } else {
-            throw '資源錯誤'
+            throw '系統錯誤'
           }
           successAlert(this.props.alert,'刪除成功')
           this.goBack()
